280f015312ced64a88687acb5e56fd4cb407855e
Justin Poliachik <jpoliachik@gmail.com>
2023-06-16 12:23:14 -0400

    updated hero styles

--- components/Homepage/Hero/Hero.tsx
+++ components/Homepage/Hero/Hero.tsx
@@ -6,6 +6,8 @@ import {
   Center,
   CardFooter,
   Box,
+  VStack,
+  Text,
 } from "@chakra-ui/react";
 import { ButtonsContainer } from "components/ButtonsContainer";
 import {
@@ -14,11 +16,46 @@ import {
   RoundsButton,
   SignupButton,
 } from "components/NavButtons";
+import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <Center h="100vh" width="100vw">
-      <Card
+    <Center
+      h={{ base: "70vh", md: "100vh" }}
+      w="100%"
+      background="linear-gradient(var(--chakra-colors-gradient-darkBlueHero), var(--chakra-colors-gradient-graybluehero)), url('https://images.unsplash.com/photo-1458560871784-56d23406c091?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80') fixed"
+      backgroundSize="cover"
+      backgroundRepeat="no-repeat"
+      backgroundPosition="center"
+    >
+      <VStack alignItems="flex-start" px={{ base: "8", md: "16", lg: "24" }}>
+        <Text fontFamily="Fraunces" fontSize={"14"}>
+          every 2 months
+        </Text>
+        <Heading as="h1" fontSize={{ base: "lg", md: "2xl", lg: "4xl" }}>
+          we all choose a song
+        </Heading>
+        <Heading as="h1" fontSize={{ base: "lg", md: "2xl", lg: "4xl" }}>
+          we all cover it
+        </Heading>
+        <Box>
+          <Heading
+            as="h1"
+            mt={"16"}
+            fontSize={{ base: "lg", md: "2xl", lg: "4xl" }}
+          >
+            everyone plays the same song
+          </Heading>
+          <Image
+            src="/pencil-underline.png"
+            layout="responsive"
+            height={"10px"}
+            width={"100%"}
+          />
+        </Box>
+      </VStack>
+
+      {/* <Card
         background="none"
         display="flex"
         alignItems="center"
@@ -46,7 +83,7 @@ export const Hero = () => {
             </Box>
           </ButtonsContainer>
         </CardFooter>
-      </Card>
+      </Card> */}
     </Center>
   );
 };
--- components/shared/Header/Header.tsx
+++ components/shared/Header/Header.tsx
@@ -5,21 +5,26 @@ import { FAQButton } from "components/NavButtons";
 
 export const Header = (): ReactElement => {
   return (
-    <Box as="header" id="header">
+    <Box
+      as="header"
+      id="header"
+      background="rgba(0,0,0,0.4)"
+      backdropBlur={"xl"}
+      backdropFilter={"blur(6px)"}
+      position="fixed"
+      top={0}
+      left={0}
+      width={"100%"}
+      zIndex="sticky"
+    >
       <Flex
         alignItems="center"
         justifyContent={{ base: "center", md: "space-between" }}
-        px="4"
-        py="2"
+        px={{ base: "4", md: "8" }}
+        py={"4"}
       >
         <Link href={"/"}>
-          <Heading
-            textStyle={"h1"}
-            as="h1"
-            fontFamily={"Fraunces"}
-            size={{ base: "sm", lg: "lg" }}
-            fontWeight="700"
-          >
+          <Heading size={{ base: "xs", lg: "md" }}>
             everyone plays the same song
           </Heading>
         </Link>
