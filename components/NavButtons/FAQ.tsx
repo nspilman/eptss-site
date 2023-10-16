1e7bfbfad0e8d8e074aed7a919ef7d9ad65148c6
Brando <bbaker.sh@gmail.com>
2023-10-15 21:31:27 -0700

    switched div to button

--- components/NavButtons/FAQ.tsx
+++ components/NavButtons/FAQ.tsx
@@ -3,12 +3,9 @@ import { Navigation } from "components/enum/navigation";
 
 export const FAQButton = () => (
     <Link href={Navigation.FAQ}>
-      <div className=" h-10 py-2 px-4 border-2 font-bold text-white border-white bg-transparent flex items-center rounded-md hover:border-yellow-200
+      <button className=" h-10 py-2 px-4 border-2 font-bold text-white border-white bg-transparent flex items-center rounded-md hover:border-yellow-200
        hover:bg-white hover:text-black hover:shadow-md hover:shadow-yellow-200 hover:cursor-pointer">
       FAQ
-      </div>
+      </button>
     </Link>
-  // <Link href={Navigation.FAQ} variant="button">
-  //   <Button>FAQ</Button>
-  // </Link>
 );
