dbeea9392578f8d68676c6782128d245e2aa46d7
Brando <bbaker.sh@gmail.com>
2023-10-15 20:58:55 -0700

    refactored the FAQ button on nav using tailwind and next/link

--- components/NavButtons/FAQ.tsx
+++ components/NavButtons/FAQ.tsx
@@ -1,8 +1,14 @@
-import { Button, Link } from "@chakra-ui/react";
+import Link from "next/link";
 import { Navigation } from "components/enum/navigation";
 
 export const FAQButton = () => (
-  <Link href={Navigation.FAQ} variant="button">
-    <Button>FAQ</Button>
-  </Link>
+    <Link href={Navigation.FAQ}>
+      <div className=" h-10 py-2 px-4 border-2 font-bold text-white border-white bg-transparent flex items-center rounded-md hover:border-yellow-200
+       hover:bg-white hover:text-black hover:shadow-md hover:shadow-yellow-200 hover:cursor-pointer">
+      FAQ
+      </div>
+    </Link>
+  // <Link href={Navigation.FAQ} variant="button">
+  //   <Button>FAQ</Button>
+  // </Link>
 );
