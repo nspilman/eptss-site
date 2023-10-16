ad73661f5091e0fc3b23ae6bf7f8a41d93f5b04c
Brando <bbaker.sh@gmail.com>
2023-10-16 11:24:20 -0700

    Added custom style for hover glow on FAQ nav button

--- components/NavButtons/FAQ.tsx
+++ components/NavButtons/FAQ.tsx
@@ -4,7 +4,7 @@ import { Navigation } from "components/enum/navigation";
 export const FAQButton = () => (
     <Link href={Navigation.FAQ}>
       <button className=" h-10 py-2 px-4 border-2 font-bold text-white border-white bg-transparent flex items-center rounded-md hover:border-yellow-200
-       hover:bg-white hover:text-black hover:shadow-md hover:shadow-yellow-200 hover:cursor-pointer">
+       hover:bg-white hover:text-black hover:shadow-NavShadow hover:cursor-pointer">
       FAQ
       </button>
     </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        NavShadow: " 0px 0px 3px 3px rgba(	246,	224,	94, .8)",
+      },
+    },
   },
   plugins: [],
 };
