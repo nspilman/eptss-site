799d237ced91fb4f38f29ec7aca313798f44863d
Brando <bbaker.sh@gmail.com>
2023-10-16 15:18:36 -0700

    removed unnecessary styles

--- components/NavButtons/FAQ.tsx
+++ components/NavButtons/FAQ.tsx
@@ -3,7 +3,7 @@ import { Navigation } from "components/enum/navigation";
 
 export const FAQButton = () => (
     <Link href={Navigation.FAQ}>
-      <button className=" h-10 py-2 px-4 border-2 font-bold text-white border-white bg-transparent flex items-center rounded-md hover:border-yellow-200
+      <button className=" h-10 py-2 px-4 border-2 font-bold text-white border-white bg-transparent flex items-center rounded-md
        hover:bg-white hover:text-black hover:shadow-NavShadow hover:cursor-pointer">
       FAQ
       </button>
