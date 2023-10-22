883d80de30c9ccd8124adc6239fa4c680e2d0f71
Brando <bbaker.sh@gmail.com>
2023-10-22 15:02:24 -0700

    removed onClick from interface

--- components/shared/Button/Button.tsx
+++ components/shared/Button/Button.tsx
@@ -4,7 +4,6 @@ import { Navigation } from 'components/enum/navigation'
 
 interface ButtonProps {
   href: Navigation;
-  onClick?: () => void;
   title: string;
 }
 const Button = ({ href, title }: ButtonProps): React.ReactElement => {
