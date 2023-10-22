4209dc2734d669f315d3eb65c38ff0da5e9a5f74
Brando <bbaker.sh@gmail.com>
2023-10-22 15:01:39 -0700

    chaged dir name from Tailwind_Btn to Button

--- components/NavButtons/FAQ.tsx
+++ components/NavButtons/FAQ.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
 import { Navigation } from "components/enum/navigation";
-import Button from "components/shared/Tailwind_Btn/Button";
+import Button from "components/shared/Button/Button";
 
 export const FAQButton = () => (<Button href={Navigation.FAQ} title="FAQ" />);
