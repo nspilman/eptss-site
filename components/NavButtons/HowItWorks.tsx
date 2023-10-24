99bcf81e32a5567cb4c94ec52cf4f356ac67995e
Brando <bbaker.sh@gmail.com>
2023-10-23 22:32:59 -0700

    refactored how it works button

--- components/NavButtons/HowItWorks.tsx
+++ components/NavButtons/HowItWorks.tsx
@@ -1,8 +1,4 @@
-import { Link, Button } from "@chakra-ui/react";
+import Button from "components/shared/Button/Button";
 import { Navigation } from "components/enum/navigation";
 
-export const HowItWorksButton = () => (
-  <Link id="learn" href={Navigation.HowItWorks} variant="button">
-    <Button>How it Works</Button>
-  </Link>
-);
+export const HowItWorksButton = () => (<Button href={Navigation.HowItWorks} title="How It Works" />);
