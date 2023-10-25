53eae69afecc0f63a1bbf63a82f424c8d71e1ee8
Brando <bbaker.sh@gmail.com>
2023-10-25 06:56:08 -0700

    Style: one lined the howItWorksButton

--- components/NavButtons/HowItWorks.tsx
+++ components/NavButtons/HowItWorks.tsx
@@ -1,6 +1,4 @@
 import { Navigation } from "components/enum/navigation";
 import { Button } from "components/shared/Button";
 
-export const HowItWorksButton = () => (
-  <Button href={Navigation.HowItWorks} title="How It Works" />
-);
+export const HowItWorksButton = () => <Button href={Navigation.HowItWorks} title="How It Works" />;
