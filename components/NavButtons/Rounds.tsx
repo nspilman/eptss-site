f07d90259af7cff8de9c8b4b0a9e718ac2ae2ec5
Brando <bbaker.sh@gmail.com>
2023-11-19 22:25:42 -0800

    one lined component

--- components/NavButtons/Rounds.tsx
+++ components/NavButtons/Rounds.tsx
@@ -1,4 +1,3 @@
 import { Button } from "components/shared/Button";
 import { Navigation } from "components/enum/navigation";
-export const RoundsButton = () => 
-  <Button href={Navigation.Rounds} title="Rounds" />
+export const RoundsButton = () => <Button href={Navigation.Rounds} title="Rounds" />;
