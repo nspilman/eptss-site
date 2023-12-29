758ffa2b270a4d5271aa14a979bc644feedde19e
Nate Spilman <nate.spilman@gmail.com>
2023-12-29 13:28:18 -0800

    adding toast error when form doesn't work

--- components/shared/FormContainer/FormContainer.tsx
+++ components/shared/FormContainer/FormContainer.tsx
@@ -1,4 +1,6 @@
 import { useSuccessState } from "components/hooks/useSuccessState";
+import { useToast } from "components/ui/use-toast";
+import { useEffect } from "react";
 import { FieldValues } from "react-hook-form";
 import { Form } from "./Form";
 import { InputType } from "./Form/types";
@@ -23,7 +25,17 @@ export function FormContainer<T extends FieldValues>({
   submitButtonText,
 }: Props<T>) {
   const [successState, setSuccessState] = useSuccessState();
-
+  const { toast } = useToast();
+  useEffect(() => {
+    if (successState === "error") {
+      toast({
+        title: "Whoops, something broke",
+        description:
+          "An error has occurred. Please try again and/or hit Nate up.",
+        variant: "destructive",
+      });
+    }
+  });
   return (
     <div className="flex items-center justify-center bg-[rgba(28,32,38,.9)] py-8 px-12 rounded-3xl w-full">
       <div className="w-[300px] sm:w-[450px] md:w-[600px] lg:w-[800px] flex items-center">
