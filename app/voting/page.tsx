c7d38f66bd017b546aa861e4ed4c81d48197b09d
Nate Spilman <nate.spilman@gmail.com>
2024-04-11 08:07:33 -0700

    sorting refactoring for voting options

--- app/voting/page.tsx
+++ app/voting/page.tsx
@@ -4,29 +4,11 @@ import {
   userParticipationProvider,
   userSessionProvider,
 } from "@/providers";
-import seedrandom from "seedrandom";
 import { PageTitle } from "@/components/PageTitle";
 import { ClientFormWrapper } from "@/components/client/Forms/ClientFormWrapper";
 import { Form } from "@/components/Form";
 import { submitVotes } from "@/actions/actions";
-import { getRoundOverrideVotes, getSignupsByRound } from "@/data-access";
 import { ActionSuccessPanel } from "@/components/ActionSuccessPanel";
-
-export interface VoteOptionModel {
-  label: string;
-  field: string;
-  link: string;
-}
-
-export interface VoteOptionEntity {
-  song: {
-    title: string;
-    artist: string;
-  };
-  song_id: number;
-  round_id: string;
-  youtube_link?: string;
-}
 
 const VotingPage = async () => {
   const {
@@ -34,22 +16,13 @@ const VotingPage = async () => {
     dateLabels: {
       covering: { opens: coveringStartLabel },
     },
-    typeOverride,
+    voteOptions,
   } = await roundProvider();
 
   const { userId } = await userSessionProvider();
 
-  const isVotingOpen = true;
-  const unsortedVoteOptions = isVotingOpen
-    ? await getVoteOptions(roundId, typeOverride as "runner_up" | undefined)
-    : [];
-
-  const voteOptions =
-    typeOverride === "runner_up"
-      ? unsortedVoteOptions
-      : seededShuffle(unsortedVoteOptions, JSON.stringify(unsortedVoteOptions));
-
   const title = `Vote for the songs you want to cover in Round ${roundId}`;
+  const { phase } = await roundProvider();
 
   const fields = [
     ...voteOptions.map((option) => ({
@@ -77,7 +50,6 @@ const VotingPage = async () => {
     },
   ];
 
-  const { phase } = await roundProvider();
   const { userRoundDetails } = await userParticipationProvider();
 
   const shouldRenderForm =
@@ -129,72 +101,3 @@ const VotingPage = async () => {
 };
 
 export default VotingPage;
-
-const getVoteOptions = async (roundId: number, typeOverride?: "runner_up") => {
-  const resultEntities:
-    | {
-        round_id: any;
-        original_round_id?: any;
-        song_id: any;
-        song: {
-          title: any;
-          artist: any;
-        }[];
-      }[]
-    | null = [];
-  if (typeOverride === "runner_up") {
-    const { data, error } = await getRoundOverrideVotes(roundId);
-    //@ts-ignore
-    data?.forEach((record) => record.song && resultEntities.push(record));
-    if (error) {
-      throw new Error(JSON.stringify(error));
-    }
-  } else {
-    const data = await getSignupsByRound(roundId);
-    //@ts-ignore
-    data?.forEach((record) => resultEntities.push(record));
-    // if (error) {
-    //   throw new Error(JSON.stringify(error));
-    // }
-  }
-
-  return (
-    resultEntities
-      ?.filter((result) => result.song_id)
-      .map((result) => {
-        return result && entityToModel(result as unknown as VoteOptionEntity);
-      }) || []
-  );
-};
-
-const entityToModel = ({
-  song,
-  song_id,
-  youtube_link,
-}: VoteOptionEntity): VoteOptionModel => {
-  const { artist, title } = song;
-  if (!artist || !title) {
-    throw new Error("artist or title is null");
-  }
-  return {
-    label: `${title} by ${artist}`,
-    field: song_id.toString(),
-    link: youtube_link || "",
-  };
-};
-
-function seededShuffle<T>(array: T[], seed: string): T[] {
-  const rng = seedrandom(seed);
-
-  // Creating a new array to avoid modifying the original
-  const newArray = array.slice();
-
-  // Implementing the Fisher-Yates shuffle algorithm
-  for (let i = newArray.length - 1; i > 0; i--) {
-    const randomIndex = Math.floor(rng() * (i + 1));
-    // Swapping elements
-    [newArray[i], newArray[randomIndex]] = [newArray[randomIndex], newArray[i]];
-  }
-
-  return newArray;
-}
