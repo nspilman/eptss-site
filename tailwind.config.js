cf76fbcf91cb3a54ceebc2724d573744e79da952
Brando <bbaker.sh@gmail.com>
2024-04-30 10:32:56 -0700

    added Shadui to proejct

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   prefix: "",
   theme: {
@@ -16,19 +17,7 @@ module.exports = {
       },
     },
     extend: {
-      fontSize: {
-        sm: ["14px", "1.25rem"],
-        base: ["16px", "1.25rem"],
-        lg: ["20px", "1.25rem"],
-        xl: ["24px", "1.25rem"],
-        "3xl": ["1.875rem", "1.75rem"],
-        header: ["36px", "1.25rem"],
-      },
       colors: {
-        themeYellow: "#fffe53",
-        bgGradientLighterBLue: "red",
-        bgGradientDarkerBLue: "red",
-        bgTransparent: "hsla(0,0%,100%,.05)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -63,17 +52,10 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      boxShadow: {
-        NavShadow: " 0px 0px 3px 3px #F6E05E",
-      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      },
-      fontFamily: {
-        fraunces: ["Fraunces", "serif"],
-        roboto: ["Roboto", "serif"],
       },
       keyframes: {
         "accordion-down": {
@@ -84,27 +66,12 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        blob: {
-          "0%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-          "33%": {
-            transform: "translate(-10px, 0px) scale(1.1)",
-          },
-          "66%": {
-            transform: "translate(10px, 0px) scale(1.1)",
-          },
-          "100%": {
-            transform: "tranlate(0px, 0px) scale(1)",
-          },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        blob: "blob 7s infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+}
