67dab90a9ac8242a2cf683ef4270d0833fb09993
Brando <bbaker.sh@gmail.com>
2024-05-04 19:57:18 -0700

    moved nav wrapper div to bottom. Tested responsiveness n dat

--- components/Navbar/BottomNav.tsx
+++ components/Navbar/BottomNav.tsx
@@ -5,7 +5,7 @@ interface TopNavProps {
   const BottomNav: React.FC<TopNavProps> = ({ userId }) => {
     // You can use the userId prop here to customize the TopNav component
     return (
-      <div className="top-nav">
+      <div className="fixed bottom-0 left-0 w-full flex bg-red-500 mx-auto sm:hidden">
         {/* Example usage of userId */}
         <p>Welcome, {userId}!</p>
         {/* Add your other top navigation elements here */}
