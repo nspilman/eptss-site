c9341ecfe98a9f20fc0c17dbeeb76e86da5c1f2b
Brando <bbaker.sh@gmail.com>
2024-05-06 15:11:01 -0700

    Bbaker/redesign responsive nav (#53)
    Co-authored-by: Nate Spilman <nate.spilman@gmail.com>

---
+++ components/Navbars/NavMenus.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import * as React from "react";
+import TopNav from "./TopNav";
+import BottomNav from "./BottomNav";
+
+interface AboutMenuRoutes {
+  title: string;
+  href: string;
+  description: string;
+}
+
+interface UserIdProp {
+  userId: string
+}
+
+const about: { title: string; href: string; description: string }[] = [
+  {
+    title: "FAQ",
+    href: "/faq",
+    description: "How does this work?",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+    description: "What's new?!",
+  },
+];
+
+
+const NavMenus: React.FC<UserIdProp> = ({ userId }) => {
+  return (
+    <>
+      <TopNav userId={userId} about={about} />
+      <BottomNav userId={userId} about={about} />
+    </>
+  );
+};
+
+export default NavMenus;
