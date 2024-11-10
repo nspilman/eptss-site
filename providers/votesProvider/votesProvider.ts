4ecac1d494ec426f3dd4b8240b6b3d5b467a5ea7
Nate Spilman <nate.spilman@gmail.com>
2024-11-10 14:02:54 -0800

    removing consle log

--- providers/votesProvider/votesProvider.ts
+++ providers/votesProvider/votesProvider.ts
@@ -13,7 +13,6 @@ export const votesProvider = async ({ roundId: roundIdProp }: Props) => {
   const votingUserIds = await getVotingUsersByRound(roundId)
   const usersInRound = await getSignupUsersByRound(roundId) as unknown as {user_id: string, users: {userId: string, email: string}}[]
   const outstandingVoters = usersInRound?.filter(user => !votingUserIds?.includes(user.user_id)).map(user => user.users?.email)
-  console.log({outstandingVoters})
 
   return { voteResults, outstandingVoters };
 };
