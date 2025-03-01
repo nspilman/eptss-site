4fc0e5ba12034c5ece961738ef686ba765eab389
Nate Spilman <nate.spilman@gmail.com>
2025-03-01 12:06:21 -0800

    fixing auth callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,11 +8,12 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = searchParams.get("next") ?? "/";
-  const redirectTo = request.nextUrl.clone();
-  redirectTo.pathname = next;
-
-  const redirectUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${redirectTo.pathname}`;
+  const next = searchParams.get("next");
+  
+  // If next is a full URL, use it as is, otherwise prepend the base URL
+  const redirectUrl = next?.startsWith('http') 
+    ? next 
+    : `${process.env.NEXT_PUBLIC_BASE_URL}${next ?? '/'}`;
 
   try {
     if (token_hash && type) {
@@ -32,13 +33,13 @@ export async function GET(request: NextRequest) {
   } catch (e) {
     // return the user to an error page with some instructions
     return NextResponse.redirect(
-      redirectUrl + `/?${TOAST_REDIRECT_KEY}=${(e as Error).message}`
+      redirectUrl + `?${TOAST_REDIRECT_KEY}=${(e as Error).message}`
     );
   }
   Sentry.captureException(
     "Login error occurred - no exception thrown. url:" + request.url
   );
   return NextResponse.redirect(
-    redirectUrl + `/?${TOAST_REDIRECT_KEY}="An error has occured"`
+    redirectUrl + `?${TOAST_REDIRECT_KEY}="An error has occured"`
   );
 }
