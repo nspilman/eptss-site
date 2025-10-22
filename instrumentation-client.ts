8e35538f035b4a7042552b03d70e3e3319c5c360
Nate Spilman <nate.spilman@gmail.com>
2025-10-21 19:55:25 -0700

    trying to get posthog building

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,7 +1,9 @@
-// instrumentation-client.ts (root of your app)
-import posthog from 'posthog-js'
+import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: '2025-05-24' // Auto-handles pageviews and pageleave events
+  api_host: "/ingest",
+  ui_host: "https://us.posthog.com",
+  defaults: '2025-05-24',
+  capture_exceptions: true, // captures exceptions using Error Tracking
+  debug: process.env.NODE_ENV === "development",
 });
